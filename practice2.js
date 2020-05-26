bc59b0d4cdf59fd40583ff32681f21b73a8d64f4
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-05-26 16:28:27 -0500

    loops

--- practice2.js
+++ practice2.js
@@ -3,36 +3,79 @@
 //     password: 123456
 // };
 
-var database = [{
-    username: "Kazim",
-    password: "123456"
-}];
+// var database = [{
+//     username: "Kazim",
+//     password: "123456"
+// }];
 
 // var database = [userInfo]
-var newsfeed = [
-    {username: "andy33",
-    timeline: "sdfsdf"
-    },
-    {username: "anne32",
-    timeline: "asdf"
-    },
-    {username: "amanda34",
-    timeline: "asds"
-    }
-]
+// var newsfeed = [
+//     {username: "andy33",
+//     timeline: "sdfsdf"
+//     },
+//     {username: "anne32",
+//     timeline: "asdf"
+//     },
+//     {username: "amanda34",
+//     timeline: "asds"
+//     }
+// ]
 
-var usernamePrompt = prompt("enter username")
-var passPrompt = prompt("enter password")
+// var usernamePrompt = prompt("enter username")
+// var passPrompt = prompt("enter password")
 
-function authentify(user, pass){
-if(user === database[0].username && 
-    pass === database[0].password){
-    console.log(newsfeed);
-} 
-else {
-alert("try again");
-}
+// function authentify(user, pass){
+// if(user === database[0].username && 
+//     pass === database[0].password){
+//     console.log(newsfeed);
+// } 
+// else {
+// alert("try again");
+// }
 
-}
+// }
 
-authentify(usernamePrompt, passPrompt);
+// authentify(usernamePrompt, passPrompt);
+
+//Loop practice
+
+var todos = [
+    "clean room",
+    "brush",
+    "exercise",
+    "study",
+    "eat"
+];
+
+// for (var i = 0; i < todos.length; i++) {
+//     // todos[i] = todos[i] + "!"; adds ! after every activity
+//     todos.pop();
+// }
+
+
+//to cross activities off from the list
+// var todosLength = todos.length;
+// for (var i = 0; i < todosLength; i++) {
+//     // todos[i] = todos[i] + "!"; adds ! after every activity
+//     todos.pop();
+// }
+
+// var counterOne = 0;
+// while (counterOne < 10){
+//     console.log(counterOne);
+//     counterOne++
+// }
+
+// var counterOne = 10;
+// while (counterOne > 0){
+//     console.log(counterOne);
+//     counterOne--
+// }
+
+// var counterTwo = 10
+// do{
+//     console.log(counterTwo)
+//     counterTwo--;
+
+// }while(counterTwo > 0)
+
